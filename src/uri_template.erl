cb6d4ca59a251b701c76e59a7ae86fd147925de8
Ryan Johnson <brillianthamologist@gmail.com>
2014-06-01 17:10:06 -0500

    Initial commit.  Level one template tests pass.

---
+++ src/uri_template.erl
@@ -0,0 +1,126 @@
+-module(uri_template).
+
+%% uri_template: uri_template library's entry point.
+
+-export([sub/2]).
+
+-type alpha() :: 16#41..16#5A | 16#61..16#7A.
+-type digit() :: 16#30..16#39.
+-type hexdig() :: digit() | 65..70 | 97..102.
+-type pct_encoded() :: 37 | hexdig().
+%% - . _ ~
+-type unreserved() :: alpha() | digit() | 45 | 46 | 95 | 126.
+%% : / ? # [ ] @
+-type gen_delims() :: 58 | 47 | 63 | 35 | 91 | 93 | 64.
+%% ! $ & ' ( ) * + , ; =
+-type sub_delims() :: 33 | 61 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 59 | 61.
+-type reserved() :: gen_delims() | sub_delims().
+-type ucschar() :: 16#A0..16#D7FF | 16#F900..16#FDCF | 16#FDF0..16#FFEF 
+	| 16#10000..16#1FFFD | 16#20000..16#2FFFD | 16#30000..16#3FFFD
+	| 16#40000..16#4FFFD | 16#50000..16#5FFFD | 16#60000..16#6FFFD
+	| 16#70000..16#7FFFD | 16#80000..16#8FFFD | 16#90000..16#9FFFD
+	| 16#A0000..16#AFFFD | 16#B0000..16#BFFFD | 16#C0000..16#CFFFD
+	| 16#D0000..16#DFFFD | 16#E1000..16#EFFFD.
+-type iprivate() :: 16#E000..16#F8FF | 16#F0000..16#FFFFD | 16#100000..16#10FFFD.
+
+%% API
+
+-spec sub(list({atom(),string()}|{atom(),list(string())}|{atom()}),string()) -> string().
+sub(_Args,[]) -> "";
+sub(Args,[${|Rem]) -> extract_expression(Args,Rem, []);
+sub(Args,[C|Rem]) -> [C|sub(Args,Rem)].
+
+%% Internals
+extract_expression(_, [], _) -> erlang:error(malformed_template);
+extract_expression(Args,[$}|Rem], Exp) -> parse_expression(Args,lists:reverse(Exp)) ++ sub(Args,Rem);
+extract_expression(Args,[H|T],Exp) -> extract_expression(Args,T,[H|Exp]).
+
+parse_expression(_Args,[]) -> "";
+parse_expression(Args, [$+|Exp]) -> process_expression({reserved, Args, variable_list(Exp)});
+parse_expression(Args, [$#|Exp]) -> process_expression({fragment, Args, variable_list(Exp) });
+parse_expression(Args, [$.|Exp]) -> process_expression({named, Args, variable_list(Exp)});
+parse_expression(Args, [$/|Exp]) -> process_expression({path, Args, variable_list(Exp)});
+parse_expression(Args, [$;|Exp]) -> process_expression({pairs, Args, variable_list(Exp)});
+parse_expression(Args, [$?|Exp]) -> process_expression({query_component, Args, variable_list(Exp)});
+parse_expression(Args, [$&|Exp]) -> process_expression({query_continuation, Args, variable_list(Exp)});
+parse_expression(_Args, [$=|_]) -> erlang:error(reserved_operator);
+parse_expression(_Args, [$,|_]) -> erlang:error(reserved_operator);
+parse_expression(_Args, [$!|_]) -> erlang:error(reserved_operator);
+parse_expression(_Args, [$@|_]) -> erlang:error(reserved_operator);
+parse_expression(_Args, [$||_]) -> erlang:error(reserved_operator);
+parse_expression(Args, Exp) -> process_expression({expansion, Args, variable_list(Exp)}). 
+
+-type expression_variable() :: {nonempty_string()} | {explode, nonempty_string()} | {prefix, nonempty_string(),integer()}.
+
+-spec variable_list(string()) -> [expression_variable()].
+variable_list(Exp) -> [varspec(Var)|| Var  <- string:tokens(Exp,",")].
+
+-spec varspec(string()) -> expression_variable().
+varspec(Var) -> modifier_level4(string:tokens(Var,":")).
+
+-spec modifier_level4(string()) -> expression_variable().
+modifier_level4([Var,Len]) -> case string:to_integer(Len) of {Int, []} when Int >= 0 ->  { prefix, Var, Int } end;
+modifier_level4([Var]) -> case lists:reverse(Var) of 
+				[$*|V] -> {explode, V};
+				_ -> {Var}
+			end.
+
+process_expression({expansion, Args, Vars}) -> string:join([ encode(process_variable(Var,Args)) || Var <- Vars  ],",");
+process_expression(Exp) -> "".
+
+process_variable({Var},Args) ->proplists:get_value(Var,Args,"");
+process_variable({explode,Var},Args) -> proplists:get_value(Var,Args,"");
+process_variable({prefix,Var,Len},Args) -> string:substr(proplists:get_value(Var,Args,""), 0, Len).
+
+-spec is_alpha(char()) -> boolean(). 
+is_alpha(C) -> 
+	(16#41 =< C andalso C =< 16#5A) orelse (16#61 =< C andalso C =< 16#7A).
+
+-spec is_digit(char()) -> boolean().
+is_digit(C) ->
+	16#30 =< C andalso C =< 16#39.
+
+-spec is_unreserved(char()) -> boolean().
+is_unreserved($-) -> true;
+is_unreserved($.) -> true;
+is_unreserved($_) -> true;
+is_unreserved($~) -> true;
+is_unreserved(C) -> is_alpha(C) orelse is_digit(C).
+
+-spec is_gen_delim(char()) -> boolean().
+is_gen_delim($:) -> true;
+is_gen_delim($/) -> true;
+is_gen_delim($?) -> true;
+is_gen_delim($#) -> true;
+is_gen_delim($[) -> true;
+is_gen_delim($]) -> true;
+is_gen_delim($@) -> true;
+is_gen_delim(_) -> false.
+
+-spec is_sub_delim(char()) -> boolean().
+is_sub_delim($!) -> true;
+is_sub_delim($$) -> true;
+is_sub_delim($&) -> true;
+is_sub_delim($') -> true;
+is_sub_delim($() -> true;
+is_sub_delim($)) -> true;
+is_sub_delim($*) -> true;
+is_sub_delim($+) -> true;
+is_sub_delim($,) -> true;
+is_sub_delim($;) -> true;
+is_sub_delim($=) -> true;
+is_sub_delim(_) -> false.
+
+-spec is_reserved(char()) -> boolean().
+is_reserved(C) ->
+	is_gen_delim(C) orelse is_sub_delim(C).
+
+-spec percent_encode(char()) -> string().
+percent_encode(C) -> case is_unreserved(C) of 
+		       	false -> binary:bin_to_list(erlang:iolist_to_binary(io_lib:format("%~2.16.0B", [C])));
+			true -> [C]
+			end.
+
+-spec encode(string()) -> string().
+encode([]) -> "";
+encode([H|T]) -> percent_encode(H) ++ encode(T).
---
+++ test/test_uri_template.erl
@@ -0,0 +1,49 @@
+-module(test_uri_template).
+-include_lib("eunit/include/eunit.hrl").
+
+level_1_test_() -> run_tests(1).
+
+run_tests(Level) ->
+	{Variables, Tests} = load_tests_for_level(Level),
+	[ create_test_fun(Template, Variables, Expected) || {Template, Expected} <- Tests ].
+
+create_test_fun(Template, Variables, Expected) ->
+	fun() -> 
+		Result = uri_template:sub(Variables,Template),
+		?debugVal(Result),
+		?debugVal(sets:to_list(Expected)),
+		?assertEqual(true, sets:is_element(Result,Expected)) 
+	end.
+
+
+%% Functions for loading and converting the tests data.
+convert_test_cases(Tests) -> [convert_test_case(Test) || Test <- Tests].
+convert_test_case([Template,Data]) -> {binary_to_list(Template), convert_expected_data(Data)}.
+
+convert_expected_data(Data) when is_binary(Data) -> sets:from_list([binary_to_list(Data)]);
+convert_expected_data(Data) when is_list(Data) -> sets:from_list([ binary_to_list(D) || D <- Data]).
+
+convert_variables(Variables) -> [convert_variable(V) || V <- Variables].
+convert_variable({Name, Value}) when is_binary(Value)  -> {binary_to_list(Name),binary_to_list(Value)};
+convert_variable({Name, Values}) when is_list(Values) -> {binary_to_list(Name), [convert_variable_list_entry(V) || V <- Values]}.
+convert_variable_list_entry({Key,Value}) -> {binary_to_list(Key),binary_to_list(Value)};
+convert_variable_list_entry(Value) when is_list(Value) -> binary_to_list(Value).
+
+load_tests_for_level(1) -> load_tests_for_name(<<"Level 1 Examples">>);
+load_tests_for_level(2) -> load_tests_for_name(<<"Level 2 Examples">>);
+load_tests_for_level(3) -> load_tests_for_name(<<"Level 3 Examples">>);
+load_tests_for_level(4) -> load_tests_for_name(<<"Level 4 Examples">>).
+load_tests_for_name(Level) ->
+	LoadedData = load_spec_test_data(),
+	{_,LevelData}  = proplists:lookup(Level,LoadedData),
+	{_,VariablesData} = proplists:lookup(<<"variables">>,LevelData),
+	{_,TestcasesData} = proplists:lookup(<<"testcases">>,LevelData), 
+	{convert_variables(VariablesData),convert_test_cases(TestcasesData)}.
+
+load_extended_test_data() -> load_data("extended-tests.data").
+load_negative_test_data() -> load_data("negative-tests.data"). 
+load_spec_test_data() -> load_data("spec-examples.data").
+data_file(FileName) -> "../test/" ++ FileName.
+load_data(FileName) -> 	
+	{ok, Bin} = file:read_file(data_file(FileName)),
+	binary_to_term(Bin).		
